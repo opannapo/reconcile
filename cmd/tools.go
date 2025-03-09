5e57e6704087a1d1c2143d85bac178bc4250da1b
opannapo <taufan.alfazri@gmail.com>
2025-03-09 21:28:47 +0700

    fix tools csv generator

--- cmd/tools.go
+++ cmd/tools.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"log"
 	"math/rand"
 	"os"
 	"path/filepath"
@@ -15,43 +16,126 @@ func main() {
 	_ = os.MkdirAll(dirPathSystem, os.ModePerm)
 	_ = os.MkdirAll(dirPathBank, os.ModePerm)
 
-	systemHeaders := []string{"TrxID", "Amount", "TransactionTime"}
-	bankHeaders := []string{"UniqueID", "Amount", "Date"}
-
 	//generate master system transaction
-	generateCSV(Option{Headers: systemHeaders, Filename: filepath.Join(dirPathSystem, "system.csv"), DateFormat: "2006-01-02T15:04:05", NumRows: 100})
-
-	//generate bank transaction
-	generateCSV(Option{Headers: bankHeaders, Filename: filepath.Join(dirPathBank, "bank_1.csv"), DateFormat: "2006-01-02", NumRows: 20})
-	generateCSV(Option{Headers: bankHeaders, Filename: filepath.Join(dirPathBank, "bank_2.csv"), DateFormat: "2006-01-02", NumRows: 30})
-	generateCSV(Option{Headers: bankHeaders, Filename: filepath.Join(dirPathBank, "bank_3.csv"), DateFormat: "2006-01-02", NumRows: 70})
+	generateCSV(Option{
+		SystemFilename: filepath.Join(dirPathSystem, "system.csv"),
+		NumRows:        100,
+		BankFilenames: [][]string{
+			{filepath.Join(dirPathBank, "bank_1.csv"), "BCA"},
+			{filepath.Join(dirPathBank, "bank_2.csv"), "MANDIRI"},
+			{filepath.Join(dirPathBank, "bank_3.csv"), "BRI"},
+		},
+	})
 }
 
+type Master struct {
+	ID     string
+	Amount float64
+	Type   string
+	Date   time.Time
+}
 type Option struct {
-	Headers    []string
-	Filename   string
-	DateFormat string
-	NumRows    int
+	SystemFilename string
+	BankFilenames  [][]string
+	NumRows        int
 }
 
 func generateCSV(opt Option) {
-	file, err := os.Create(opt.Filename)
+	systemHeaders := []string{"TrxID", "Amount", "Type", "TransactionTime"}
+	bankHeaders := []string{"UniqueID", "Amount", "Date"}
+
+	//writer file system
+	sysFile, err := os.Create(opt.SystemFilename)
 	if err != nil {
 		fmt.Println("Error creating system transaction file:", err)
 		return
 	}
-	defer file.Close()
-
-	writer := csv.NewWriter(file)
+	defer sysFile.Close()
+	writer := csv.NewWriter(sysFile)
 	defer writer.Flush()
 
-	writer.Write(opt.Headers)
+	//writer bank
+	writersBank := []*csv.Writer{}
+	for _, filename := range opt.BankFilenames {
+		f, err := os.Create(filename[0])
+		if err != nil {
+			fmt.Println("Error creating system transaction file:", err)
+			return
+		}
+		defer f.Close()
+
+		w := csv.NewWriter(f)
+		writersBank = append(writersBank, w)
+	}
+	defer func() {
+		for _, wb := range writersBank {
+			wb.Flush()
+		}
+	}()
+
+	//generate file system
+	writer.Write(systemHeaders)
+	sysTxTypes := []string{"DEBIT", "CREDIT"}
+	master := []Master{}
 	for i := 0; i < opt.NumRows; i++ {
-		trxID := fmt.Sprintf("SYS-%06d", i+1)
+		trxID := fmt.Sprintf("SYS%06d", i+1)
 		amount := rand.Float64() * 100
 		date := time.Now().AddDate(0, 0, -rand.Intn(10))
-		writer.Write([]string{trxID, fmt.Sprintf("%.1f", amount), date.Format(opt.DateFormat)})
+		txType := sysTxTypes[rand.Intn(len(sysTxTypes))]
+		master = append(master, Master{
+			ID:     trxID,
+			Amount: amount,
+			Type:   txType,
+			Date:   date,
+		})
+
+		writer.Write([]string{
+			trxID,
+			fmt.Sprintf("%.1f", amount),
+			txType,
+			date.Format("2006-01-02T15:04:05"),
+		})
+	}
+
+	//generate file bank
+	bankSizes := generateRandomSizes(opt.NumRows, len(opt.BankFilenames))
+	log.Println("bankSizes", bankSizes)
+	start := 0
+	for i, size := range bankSizes {
+		end := start + size - 1
+		fmt.Printf("Bank %d len=%d. Start %d - end %d\n", i, size, start, end)
+
+		partMaster := master[start:end]
+		writersBank[i].Write(bankHeaders)
+		for ii, m := range partMaster {
+			bankName := fmt.Sprintf("%s%d", opt.BankFilenames[i][1], ii)
+			date := m.Date.Format("2006-01-02")
+			amount := m.Amount
+			if m.Type == "DEBIT" {
+				amount = -amount
+			}
+			writersBank[i].Write([]string{
+				bankName,
+				fmt.Sprintf("%f", amount),
+				date,
+			})
+		}
+		start = end + 1
 	}
 
 	return
+}
+
+func generateRandomSizes(totalLen, childCount int) []int {
+	rand.Seed(time.Now().UnixNano())
+	sizes := make([]int, childCount)
+	remaining := totalLen
+	for i := 0; i < childCount-1; i++ {
+		sizes[i] = rand.Intn(remaining/(childCount-i)) + 1
+		remaining -= sizes[i]
+	}
+
+	sizes[childCount-1] = remaining
+
+	return sizes
 }
